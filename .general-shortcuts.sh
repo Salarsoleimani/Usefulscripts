c573039ea180834a60832827522b3179977701a4
Salar Soleimani <s.s_m1983@yahoo.com>
2020-04-18 17:37:44 -0400

    [add]

---
+++ .general-shortcuts.sh
@@ -0,0 +1,52 @@
+alias general='curl general.salarsoleimani.com'
+
+function mkd() {
+    mkdir -p "$@" && cd "$@"
+}
+# List only directories
+alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
+
+# Show/hide hidden files in Finder
+alias show="defaults write com.apple.finder AppleShowAllFiles -bool true && killall Finder"
+alias hide="defaults write com.apple.finder AppleShowAllFiles -bool false && killall Finder"
+
+# Hide/show all desktop icons (useful when presenting)
+alias hidedesktop="defaults write com.apple.finder CreateDesktop -bool false && killall Finder"
+alias showdesktop="defaults write com.apple.finder CreateDesktop -bool true && killall Finder"
+
+# Safely trash files
+alias rm=trash
+
+# Easier navigation: .., ..., ...., ....., ~ and -
+alias ..="cd .."
+alias ...="cd ../.."
+alias ....="cd ../../.."
+alias .....="cd ../../../.."
+alias -- -="cd -"
+
+# npm
+alias npmi="sudo npm install"
+
+# Get week number
+alias week='date +%V'
+
+# Stopwatch
+alias timer='echo "Timer started. Stop with Ctrl-D." && date && time cat && date'
+
+#upgrade homebrew
+alias brewup='brew update && brew upgrade && brew cleanup'
+
+# IP addresses
+alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
+alias localip="ipconfig getifaddr en0"
+alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# Enhanced WHOIS lookups
+alias whois="whois -h whois-servers.net"
+
+# View HTTP traffic
+alias sniff="sudo ngrep -d 'en1' -t '^(GET|POST) ' 'tcp and port 80'"
+alias httpdump="sudo tcpdump -i en1 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
+
+# Check internet
+alias checknet = "ping 8.8.8.8"
---
+++ .xcode-shortcuts.sh
@@ -0,0 +1,146 @@
+alias xcode='curl xcode.salarsoleimani.com'
+
+cleandd() {
+    rm -rf ~/Library/Developer/Xcode/DerivedData
+    echo "Removed all derived data."
+}
+
+# Ring the terminal bell, and put a badge on Terminal.app’s Dock icon
+# (useful when executing time-consuming commands)
+alias badge="tput bel"
+
+# GIT
+#straight up git status alias
+alias gs='git status'
+#status, but displayed in a more friendly/clean way
+alias gss='git status -s'
+
+#add all files
+alias gaa='git add -A'
+
+#git pull and push aliases
+alias gp='git pull'
+alias gph='git push'
+
+#pull a specific branch/remote
+function gp {
+  gp "$1" "$2"
+}
+
+#pull a specific branch/remote
+function gph {
+  gph "$1" "$2"
+}
+
+#commit all files with message, and then display status
+function gc {
+  gaa
+  git commit -m "$1"
+  gs
+}
+
+#show local and current branch
+alias gb='git branch'
+#show remote branches
+alias gbr='git branch -r'
+#create new branch
+alias gbn='git checkout -b'
+
+#checkout branch
+alias gck='git checkout'
+
+#remove local branch
+function gbdel {
+  git branch -D "$1"
+}
+#delete remote branch
+function gbrdel {
+  git push origin :"$1"
+}
+
+#pull a specific branch/remote
+function gt {
+  git tag -m "" $1
+  git push origin --tags
+}
+
+#diff in color
+alias gdiff='git diff --color-words'
+
+#get a list of conflicts
+alias conflicts='git diff --name-only --diff-filter=U'
+
+#remove any files after adding to gitignore
+alias gri='git ls-files --ignored --exclude-standard | xargs git rm'
+
+
+alias gclean='git gc --prune=now && git remote prune origin'
+
+#log of commits
+alias glog='git log --graph --oneline --all --decorate'
+
+#reset to head
+alias greset='git reset --hard'
+# reset to head and remove all untracked files (including npm installs)
+alias greseteverything='git clean -d -x -f; git reset --hard'
+
+
+
+if type git-up -t > /dev/null 2>&1; then
+  alias gup='git-up'
+  alias gupp='git-up && git push'
+else
+  alias gpr='git pull --rebase'
+  alias gprp='gup && git push'
+fi
+
+
+#amend a commit by adding more files or changing the commit messge
+function goops {
+  git commit -a --amend -C HEAD
+  gs
+}
+
+
+function ghelp {
+  echo "
+- - - - - - - - - - - - - -
+Git Convenience Shortcuts:
+- - - - - - - - - - - - - -
+ghelp - List all Git Convenience commands and prompt symbols
+gp - Git Pull
+gup - Pull (via rebase)
+gph - Git Push
+gaa - Add all changes (including untracted files) to staging
+gc "Message" - Commit all new files & changes with message
+goops - Add changes to previous commit & edit comessage
+- - - - - - - - - - - - - -
+gt v0.0.0 - Tag and push tag to remote
+gb - Show local branches
+gbn - Create new branch
+gbr – Show remote branches
+gbdel - Delete local branch
+gbrdel - Delete remote branch
+- - - - - - - - - - - - - -
+gs - git status
+gss - Visually better git status
+glog - Decorated & graphed log
+gdiff - A word-diff of changes
+gclean - Compress & garbage collect data store
+greset - Hard branch reset
+greseteverything - Hard reset and remove all untracked files
+conflicts - Display list of conflicts
+- - - - - - - - - - - - - -
+Prompt Symbols:
+- - - - - - - - - - - - - -
+The prompt shows the current branch & among other helpful things:
+*  - Uncommitted changes
++  - Staged changes
+%  - Untracked files
+<  - You're behind the origin
+>  - You're ahead of the origin
+<> - You've diverged from the origin
+=  - You're up-to-date with the origin
+- - - - - - - - - - - - - -
+"
+}
